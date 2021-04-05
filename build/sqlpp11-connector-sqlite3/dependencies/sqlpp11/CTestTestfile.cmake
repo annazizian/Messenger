ecd88025aecf0e763e215d553e1676b67f72d0cf
Anna <33285473+annazizian@users.noreply.github.com>
2021-04-05 12:45:19 +0400

    Delete build directory

--- build/sqlpp11-connector-sqlite3/dependencies/sqlpp11/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Anna/Desktop/Messenger/sqlpp11-connector-sqlite3/dependencies/sqlpp11
-# Build directory: /mnt/c/Users/Anna/Desktop/Messenger/build/sqlpp11-connector-sqlite3/dependencies/sqlpp11
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../../../_deps/sqlpp11_core-build")
